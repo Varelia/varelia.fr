a497e2575e01ee03e407a0efa0b4253ad91cec0f
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-01 21:09:42 +0200

    refactor: change App declaration for typescript syntax

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import './style/index.scss'
 
 
-function App() {
-  return (
-      <>
-      </>
-  )
-}
+const App: React.FC = () => (
+  <>
+  </>
+)
 
 export default App
