caca8834c787703a1ce5dabf468d6c9a8b6630ef
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-02 15:48:08 +0200

    feat: add fontawesome implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Header from './components/header'
 import './style/index.scss'
-
+import './vendor/fontawesome'
 
 const App: React.FC = () => (
   <>
+    <Header />
   </>
 )
 
