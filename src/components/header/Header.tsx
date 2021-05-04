d7b46f1d18d4ffc028daa8abe481568c4f38298b
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-04 22:55:24 +0200

    add varelia logo at the center of the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import logo from '../../assets/images/varelia.png'
 
 const Header: React.FC = () => (
     <header className="main-header">
         <NavBar />
+        <section className="main-header--body">
+            <img src={logo} alt="Varelia"/>
+        </section>
     </header>
 )
 
