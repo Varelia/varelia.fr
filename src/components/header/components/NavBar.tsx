ddfa8f8cf8016a5c6ddedc43d3dba7843cbb3767
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-04 22:23:15 +0200

    add navbar

--- src/components/header/components/NavBar.tsx
+++ src/components/header/components/NavBar.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import MobileMenu from './MobileMenu'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 const NavBar: React.FC = () => (
     <nav className="header-navbar">
+        <div className="header-navbar--menu">
+            <ul>
+                <li className="header-navbar--menu--mobile">
+                    <label htmlFor="close-mobile-menu">
+                        <FontAwesomeIcon icon={['fas', 'bars']} /> Accueil
+                    </label>
+                </li>
+            </ul>
+        </div>
 
-        <input id="close-mobile-menu" type="checkbox"/>
+        <input id="close-mobile-menu" type="checkbox" hidden/>
         <MobileMenu />
     </nav>
 )
