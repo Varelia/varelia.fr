b5ea7ee27372c8ba277e817c96f80cfbca097f44
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-05 13:30:47 +0200

    add new icons from fontawesome for new items in header

--- src/vendor/fontawesome.ts
+++ src/vendor/fontawesome.ts
@@ -1,7 +1,12 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faBars, faGamepad, faUsers, faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faDiscord } from '@fortawesome/free-brands-svg-icons'
 
 library.add(
     faTimes,
-    faBars
+    faBars,
+    faGamepad,
+    faUsers,
+    faCircle,
+    faDiscord
 )
