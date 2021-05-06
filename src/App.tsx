1899f54abdd3574b774702dc1f1f8231d25cc54a
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-06 12:36:55 +0200

    feat: implement react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 import Header from './components/header'
 import './style/index.scss'
 import './vendor/fontawesome'
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from 'react-router-dom'
 
 const App: React.FC = () => (
-  <>
-    <Header />
-  </>
+    <BrowserRouter>
+        <Header />
+
+        <Switch>
+            <Route path="/">
+
+            </Route>
+        </Switch>
+    </BrowserRouter>
 )
 
 export default App
