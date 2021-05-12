ca85d7fb35c45d39615565e11cf2448f4f207d54
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-12 15:23:35 +0200

    add test article

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Container, {ContainerItem} from '../../components/container'
+import Card from '../../components/card'
+import Article from '../../components/article'
 
 const Home: React.FC = () => (
     <Container>
         <ContainerItem>
+            <Article title="VARÉLIA SAISON 3 — RÉSURRECTION"/>
         </ContainerItem>
         <ContainerItem>
+            <Card>
+                <></>
+            </Card>
         </ContainerItem>
     </Container>
 )
