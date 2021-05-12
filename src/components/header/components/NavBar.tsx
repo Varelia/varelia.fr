da0da9bec7365cc3764474d7ef8157bdb80600df
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-12 12:45:06 +0200

    add icon and hover effect to navbar items

--- src/components/header/components/NavBar.tsx
+++ src/components/header/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MobileMenu from './MobileMenu'
-import { Link } from 'react-router-dom'
-import { MenuTwoTone, ShoppingBasketTwoTone } from '@material-ui/icons'
+import {Link} from 'react-router-dom'
+import {HeadsetMicTwoTone, HomeTwoTone, HowToVoteTwoTone, MenuTwoTone, ShoppingBasketTwoTone} from '@material-ui/icons'
 
 const NavBar: React.FC = () => (
     <nav className="header-navbar">
@@ -12,9 +12,9 @@ const NavBar: React.FC = () => (
                         <MenuTwoTone /> Accueil
                     </label>
                 </li>
-                <li><Link to="/">Accueil</Link></li>
-                <li><Link to="/vote">Vote</Link></li>
-                <li><Link to="/discord">Discord</Link></li>
+                <li><Link to="/"><HomeTwoTone />Accueil</Link></li>
+                <li><Link to="/vote"><HowToVoteTwoTone />Vote</Link></li>
+                <li><Link to="/discord"><HeadsetMicTwoTone />Discord</Link></li>
             </ul>
             <a className="header-navbar--menu--shop-btn" href="/#"><ShoppingBasketTwoTone /> Boutique</a>
         </div>
