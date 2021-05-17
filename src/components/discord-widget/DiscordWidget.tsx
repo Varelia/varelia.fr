d36c0b8a3f0eb12c3b0db286f05da09da7cb0c4e
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-17 14:09:37 +0200

    fix: add title attribute

--- src/components/discord-widget/DiscordWidget.tsx
+++ src/components/discord-widget/DiscordWidget.tsx
@@ -10,9 +10,13 @@ const DiscordWidget: React.FC<DiscordWidgetImpl> = ({id, theme = "dark"}) => (
     <Card>
         <CardTitle title="Discord" />
         <CardBody>
-            <iframe src={`https://discord.com/widget?id=${id}&theme=${theme}`} height="500"
-                    allowTransparency={true} frameBorder="0"
-                    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
+            <iframe
+                title="Discord de Varelia"
+                src={`https://discord.com/widget?id=${id}&theme=${theme}`}
+                height="500"
+                allowTransparency={true}
+                frameBorder="0"
+                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" />
         </CardBody>
     </Card>
 )
