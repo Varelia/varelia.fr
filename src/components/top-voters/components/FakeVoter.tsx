396ba411726055391a6ed4d0b10ca9303f7cc158
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-21 23:10:56 +0200

    create fake voter which is used when data is loading

---
+++ src/components/top-voters/components/FakeVoter.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FakeVoter: React.FC = () => (
+    <div className="voter voter--fake">
+        <div className="voter--fake--head">
+        </div>
+        <div className="voter--body">
+            <p>&nbsp</p>
+            <p>&nbsp</p>
+        </div>
+    </div>
+)
+
+export default FakeVoter
