97573821c8fab207008bd2b3b6150afada502dba
lucasmrcr <78366747+lucasmrcr@users.noreply.github.com>
2021-05-21 23:11:34 +0200

    implement api

--- src/components/top-voters/TopVoters.tsx
+++ src/components/top-voters/TopVoters.tsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Card, {CardBody, CardTitle} from '../card'
 import Voter from './components/Voter'
+import {default as VoterModel} from '../../adapters/api/entity/Voter'
+import {fetchVotes} from '../../adapters/api'
+import FakeVoter from './components/FakeVoter'
 
-const TopVoters: React.FC = () => (
-    <Card>
-        <CardTitle title="Meilleurs Voteurs" />
+const TopVoters: React.FC = () => {
+    const [voters, setVoters] = useState<VoterModel[]>([])
+
+    useEffect(() => {
+        fetchVotes(5).then(response => setVoters(response.data as VoterModel[]))
+    }, [])
+
+    return <Card>
+        <CardTitle title="Meilleurs Voteurs"/>
         <CardBody className="voters">
-            <Voter nickname="Lucas__Lks" position={1} votes={456} />
-            <Voter nickname="Lakorah" position={2} votes={423} />
-            <Voter nickname="Pixu_" position={3} votes={348} />
-            <Voter nickname="Yxalit" position={4} votes={256} />
-            <Voter nickname="TryBack_" position={5} votes={0} />
+            {
+                !!voters.length ?
+                    voters.map(voter => (
+                        <Voter nickname={voter.nickname} votes={voter.votes} position={voter.id} />
+                    )) :
+                    new Array(5).fill(<FakeVoter />)
+            }
         </CardBody>
     </Card>
-)
+}
 
 export default TopVoters
